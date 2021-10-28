846270582a80ae876ee8e8518c2b907645cb5046
mustafa <mustafa@devtagon.com>
2021-10-28 17:04:17 +0300

    database code refactor

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-
 from fastapi import FastAPI, status
-import uvicorn
-
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-# Create a sqlite engine instance
-engine = create_engine("sqlite:///todo.db")
-
-# Create a DeclarativeMeta instance
-Base = declarative_base()
-
-# Define To Do class inheriting from Base
-class ToDo(Base):
-    __tablename__ = 'todos'
-    id = Column(Integer, primary_key=True)
-    task =  Column(String(50))
+from database import Base, engine
 
 # Create the database
 Base.metadata.create_all(engine)
