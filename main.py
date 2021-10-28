9b02e2a4dd986d8ce404a4ca92337bac0c68df2e
mustafa <mustafa@devtagon.com>
2021-10-28 16:44:10 +0300

    first fake app

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 
+from fastapi import FastAPI
+import uvicorn
 
-if __name__ == "__main__":
-    print("welcome to main")
-    print("a second line")
-    
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "todooo"
+
+@app.post("/todo")
+def create_todo():
+    return "create todo item"
+
+@app.get("/todo/{id}")
+def read_todo(id: int):
+    return "read todo item with id {id}"
+
+@app.put("/todo/{id}")
+def update_todo(id: int):
+    return "update todo item with id {id}"
+
+@app.delete("/todo/{id}")
+def delete_todo(id: int):
+    return "delete todo item with id {id}"
+
+@app.get("/todo")
+def read_todo_list():
+    return "read todo list"
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True)
